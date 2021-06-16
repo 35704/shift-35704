b32beea619cea230ef5ddf8d07dc1964f71fe50c
35704 <try.try.buta.04190430@yahoo.ne.jp>
2021-06-16 18:48:21 +0900

    保存まではできた

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,4 +1,27 @@
 class ShiftsController < ApplicationController
+
   def index
+    @shifts = Shift.all
   end
+
+  def new
+    @shift = Shift.new
+  end
+
+  def create
+    @shift = Shift.new(shift_params)
+    @shift.save
+    redirect_to root_path
+  end
+
+  def show
+    @shift
+  end
+
+  private
+
+  def shift_params
+    params.require(:shift).permit(:title, :content, :start_time)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_051942) do
+ActiveRecord::Schema.define(version: 2021_06_16_085832) do
+
+  create_table "shifts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "start_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
