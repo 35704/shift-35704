e3fcf2380d5f449e1ea91ea563087ad568135b67
35704 <try.try.buta.04190430@yahoo.ne.jp>
2021-06-21 15:50:28 +0900

    管理者の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_18_052411) do
+ActiveRecord::Schema.define(version: 2021_06_21_035250) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_06_18_052411) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  first_name: "管理者",
+  last_name: "権限",
+  email: "test@test.com",
+  password: "a22222",
+  password_confirmation: "a22222",
+  admin: true
+)
